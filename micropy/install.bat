c3db0bc805a62179c73db77eb93948e8a8b6f74f
MisterSilvereagle <MisterSilvereagle@gmx.de>
2020-12-17 18:13:06 +0100

    deleted the self-downloadable files

--- micropy/install.bat
+++ micropy/install.bat
@@ -1,8 +1,8 @@
 set /p COMPORT=which port is your ESP32? [eg. COM7]:
 
-python3 ./esptool-3.0/esptool.py --chip esp32 --port %COMPORT% erase_flash
+python3 ./esptool/esptool.py --chip esp32 --port %COMPORT% erase_flash
 
-python3 ./esptool-3.0/esptool.py --chip esp32 --port %COMPORT% --baud 460800 write_flash -z 0x1000 esp32-idf3-20200902-v1.13.bin
+python3 ./esptool/esptool.py --chip esp32 --port %COMPORT% --baud 460800 write_flash -z 0x1000 esp32-idf3-*.bin
 
 echo "install success"
 
--- micropy/install.sh
+++ micropy/install.sh
@@ -5,9 +5,9 @@
 echo "which /dev/tty is your esp32? [eg. /dev/ttyUSB0]:"
 read ttyvar
 
-python3 ./esptool-3.0/esptool.py --chip esp32 --port $ttyvar erase_flash
+python3 ./esptool/esptool.py --chip esp32 --port $ttyvar erase_flash
 
-python3 ./esptool-3.0/esptool.py --chip esp32 --port $ttyvar --baud 460800 write_flash -z 0x1000 esp32-idf3-20200902-v1.13.bin
+python3 ./esptool/esptool.py --chip esp32 --port $ttyvar --baud 460800 write_flash -z 0x1000 esp32-idf3-*.bin
 
 echo "install success"
 
